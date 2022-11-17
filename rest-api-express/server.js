a3b1ccac23db9ab0a4f2fe10cbd842cbfc1325f3
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-11-17 11:54:18 +0530

    rest api in ExpressJs

---
+++ rest-api-express/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { readFile } from "fs";
+import path from "path";
+
+const app = express();
+const port = 3000;
+const __dirname = path.resolve();
+
+app.get("/", (req, res) => {
+  res.end("Welcome to my REST api");
+});
+
+app.get("/show_data", (req, res) => {
+  readFile(__dirname + "/" + "data.json", "utf8", (err, data) => {
+    res.end(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
